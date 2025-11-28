8029588345268f0625a82f332d4b7977ae04e18d
Paolo Leandro Pinca <164370429+Wujipao@users.noreply.github.com>
2025-11-28 11:02:14 +0800

    Laboratory 7 full files.

---
+++ lab-7/lab-7/src/main/java/com/pinca/lab_7/model/Product.java
@@ -0,0 +1,16 @@
+package com.pinca.lab_7.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private long ID;
+    private String name;
+    private double price;
+}
